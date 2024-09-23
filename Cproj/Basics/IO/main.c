985eaf7202e5334da57384ddbd63d9c3d4602c5d
Pedro Jesus <capitalistpk@gmail.com>
2024-09-23 11:24:23 +0100

    init

---
+++ Cproj/Basics/IO/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("Enter two integers: ");
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    printf("%d, %d", a, b);
+    return 0;
+}
---
+++ Cproj/Basics/controll/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    if (a > 1) {
+        printf("The number is greater than 1\n");
+    } else {
+        printf("The number is not greater than 1\n");
+    }
+}
